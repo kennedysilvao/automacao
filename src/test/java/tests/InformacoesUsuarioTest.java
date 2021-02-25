ed2d8ac62effe52345a74af3365cf79b1f1733fa
Kennedy <kennedysilva20081@gmail.com>
2021-02-25 06:47:05 -0300

    att project

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,16 +14,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTest {
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario() throws InterruptedException {
+    private WebDriver navigator;
+
+    @Before
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver\\chromedriver.exe");
         // Abrindo navegador
-        WebDriver navigator = new ChromeDriver();
+        navigator = new ChromeDriver();
         navigator.manage().window().maximize();
         navigator.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         // Navegando para a página do Taskit !
         navigator.get("http://www.juliodelima.com.br/taskit");
-
+    }
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
         // clicar no sign-in
         WebElement linkSignIn = navigator.findElement(By.linkText("Sign in"));
         linkSignIn.click();
@@ -34,13 +40,22 @@ public class InformacoesUsuarioTest {
         // clicar no link SIGN IN
         signInBox.findElement(By.linkText("SIGN IN")).click();
         // validar que dentro do elemento com class 'me' possui o texto hi,kennedy
-        WebElement me = navigator.findElement(By.className("me"));
-        String txtElementMe = me.getText();
-        assertEquals("Hi, kennedy", txtElementMe);
+//        WebElement me = navigator.findElement(By.className("me"));
+//        String txtElementMe = me.getText();
+//        assertEquals("Hi, kennedy", txtElementMe);
 
+
+        // clicar em um link que possui a class "me"
+
+        // Clicar em um link com o texto More data about you
+
+        // Clicar no btn que tem o data-target = xpath
+
+
+    }
+    @After
+    public void tearDown() {
         // close the navigator
         navigator.quit();
-
-
     }
 }
